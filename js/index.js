3c012fd2f6956921aae2bcc5a510fabd8a03934b
olucasbrum <olucasbrum@gmail.com>
2023-02-22 15:16:29 -0300

    fix: refactoring and fixed bugs :recycle:

--- js/index.js
+++ js/index.js
@@ -1,42 +1,41 @@
-import { questions } from './question.js'
+import { questions } from './question.js';
 
-function revealAnswer() {
-  let answer = document.querySelector('.txt')
+const flashCards = {
+  revealAnswer() {
+    let answer = document.querySelector('.txt');
 
-  answer.classList.toggle('blur')
-}
+    answer.classList.toggle('blur');
+  },
 
-function nextQuestion(next) {
-  let question = document.querySelector('.card-question')
+  nextQuestion(next) {
+    let question = document.querySelector('.card-question');
 
-  question.innerHTML = ''
+    question.innerHTML = '';
 
-  let newQuestion = document.createElement('div')
+    let newQuestion = document.createElement('div');
 
-  newQuestion.classList.add(
-    'card-question',
-    'animate__animated',
-    'animate__backInRight'
-  )
+    newQuestion.classList.add('card-question', 'animate__animated', 'animate__backInRight');
 
-  newQuestion.innerHTML = `
-  <div class="tt">
-  <h1>O que é ${next.title}?</h1>
-</div>
+    newQuestion.innerHTML = `
+      <div class="tt">
+      <h1>O que é ${next.title}?</h1>
+      </div>
 
-<div class="txt blur">
-  <p>
-   ${next.description}
-  </p>
-</div>
-  `
-  question.appendChild(newQuestion)
-}
+      <div class="txt blur">
+      <p>
+        ${next.description}
+      </p>
+      </div>
+    `;
+    question.appendChild(newQuestion);
+  },
 
-function srcQuestion() {
-  let value = Math.floor(Math.random() * questions.length)
-  let anyQuestion = questions[value]
-  nextQuestion(anyQuestion)
-}
+  srcQuestion() {
+    let value = Math.floor(Math.random() * questions.length);
+    let anyQuestion = questions[value];
+    flashCards.nextQuestion(anyQuestion);
+  },
+};
 
-srcQuestion()
+flashCards.srcQuestion();
+window.flashCards = flashCards;
